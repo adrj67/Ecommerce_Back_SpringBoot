44ce93c302503d3485a8e0f48206ad8d03a586d6
Adrian Rojo <adrj67@gmail.com>
2024-06-13 12:37:52 +0100

    mostrar informacion del producto de la pagina productohome

---
+++ src/main/java/com/Springecommerce/controller/HomeController.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Springecommerce.controller;
+
+import com.Springecommerce.model.Producto;
+import com.Springecommerce.service.ProductoService;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author adrj
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    
+    private final Logger log=LoggerFactory.getLogger(HomeController.class);
+    
+    @Autowired
+    private ProductoService productoService;
+    
+    @GetMapping("")
+    public String home(Model model){
+        
+        model.addAttribute("productos", productoService.findAll());
+        
+        return "usuario/home";
+    }
+    
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id, Model model){
+        log.info("Id producto enviado como parametro {}", id);
+        Producto producto = new Producto();
+        Optional<Producto> productoOptional = productoService.get(id);
+        producto = productoOptional.get();
+        
+        model.addAttribute("producto", producto);
+        
+        return "usuario/productohome";
+    }
+    
+}
